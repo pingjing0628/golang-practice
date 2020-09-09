b610954820a4ce9720fe7d7d4e9dce3621147fcd
Yu <yuwang@starlux-airlines.com>
2020-09-09 23:42:08 +0800

    add 8.2

---
+++ 8.2_ConcurrencyClock/Ex_8.1/main.go
@@ -0,0 +1,58 @@
+package Ex_8_1
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+var port int
+
+// Package flag implements command-line flag parsing.
+// Bind the flag to a variable using the IntVar() functions.
+func init()  {
+	flag.IntVar(&port, "port", 7000, "port number")
+}
+
+func main()  {
+	flag.Parse()
+
+	server := fmt.Sprintf("localhost:%d", port)
+
+	listener, err := net.Listen("tcp", server)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Listening at localhost:%d\n", port)
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Print(err) // Ex: connection aborted
+			continue
+		}
+
+		go handleConn(conn) // handle connections concurrently
+	}
+}
+
+// 處理用戶端連線
+func handleConn(c net.Conn)  {
+	defer c.Close()
+
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+
+		if err != nil {
+			return // ex: client disconnected
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ 8.2_ConcurrencyClock/clock1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+// 週期性輸出時間的 TCP 伺服器
+// Print out current time each second
+func main()  {
+	listener, err := net.Listen("tcp", "localhost:7000")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		// Accept 會阻斷直到有連線請求進來
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Print(err) // Ex: give up to connect
+			continue
+		}
+
+		handleConn(conn) // Deal with one connection once a time
+	}
+}
+
+// 處理用戶端連線
+func handleConn(c net.Conn)  {
+	defer c.Close()
+
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+
+		if err != nil {
+			return // ex: user failed connect
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
